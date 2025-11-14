877fd9342cda5f8eae994329938e8add52ba008f
yubrani <yag24001@byui.edu>
2025-11-14 02:56:44 -0300

    final final commit

--- routes/products.js
+++ routes/products.js
@@ -6,6 +6,7 @@ const productsController = require('../controllers/products');
 router.get('/', productsController.getAll);
 
 router.get('/:id', productsController.getSingle);
+
 router.post('/', productsController.createProduct);
 
 router.put('/:id', productsController.updateProduct);
